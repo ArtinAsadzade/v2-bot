c461492a2d0ac5c2f591ab3387a1ea1a015df2a7
ArtinAsadzade <artinasadzade7@gmail.com>
2026-06-06 00:01:06 +0330

    Update

--- infra/docker/api/docker-entrypoint.sh
+++ infra/docker/api/docker-entrypoint.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
 set -e
 
-cd apps/api
+echo "[api] Running Prisma migrations..."
 
-echo "[api] Applying Prisma migrations..."
-../../node_modules/.bin/prisma migrate deploy --schema prisma/schema.prisma
+npx prisma migrate deploy --schema=./prisma/schema.prisma
 
 echo "[api] Running database seed..."
 node dist/prisma/seed.js
-
-cd ../..
 
 echo "[api] Starting server..."
 exec "$@"
